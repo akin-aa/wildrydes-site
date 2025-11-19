5372be3a29222818fbd87ab60e4b108edbf4a7e8
akin-aa <akinajayi.aa@gmail.com>
2025-11-19 22:03:18 +0200

    Update Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_Q6HKHPVTr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2lteq08q6bgj8ac4qh76aa6obj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Qmn9R2D0x', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1caa1i1jingg0vgl78b1rp4t8p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
